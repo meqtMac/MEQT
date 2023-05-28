2c13b2b4cbaf4b38855652755e0a48703223eda2
meqt <meqt@foxmail.com>
2023-05-28 13:54:41 +0800

    remove unsafe swift compiler flag

--- Package.swift
+++ Package.swift
@@ -26,11 +26,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MEQT",
-            dependencies: [],
-            swiftSettings: [
-                .unsafeFlags(["-O"])
-            ]
-        ),
+            dependencies: []
+       ),
         .testTarget(
             name: "MEQTTests",
             dependencies: ["MEQT"],
